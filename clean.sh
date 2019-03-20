d1cd8b5b45ff5d46d9a2dcc388d684bcfc8a65d0
Christophe HAMERLING <chamerling@linagora.com>
2019-03-20 14:15:55 +0100

    Add esn

--- clean.sh
+++ clean.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker rm esn_redis esn_elasticsearch esn_mongo esn_james_elasticsearch esn_cassandra esn_rabbitmq esn_sabre
+docker rm esn_redis esn_elasticsearch esn_mongo esn_james_elasticsearch esn_cassandra esn_rabbitmq esn_sabre esn
